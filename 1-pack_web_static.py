93c26db8aa9ae172014a3a14173e44dc157290b3
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-27 23:30:51 +0000

    Update: '1-pack_web_static.py' modified

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Creates an archive from the content of web_static folder """
-from fabric.api import local
+from fabric.api import *
 from datetime import datetime
 
 
@@ -8,10 +8,10 @@ def do_pack():
     """ Creates archive of the web_static folder in
     versions/web_static_<year><month><day><hour><minute><second>.tgz"""
     created_at = datetime.now()
-    archive_name = f"web_static_{created_at.strftime('%Y%m%d%H%M%S')}.tgz"
+    archive_path = f"versions/web_static_{created_at.strftime('%Y%m%d%H%M%S')}.tgz"
     local("mkdir -p versions")
-    result = local(f"tar -cvzf versions/{archive_name} web_static/")
+    print(f"Packing web_static to {archive_path}")
+    result = local(f"tar -cvzf {archive_path} web_static")
     if result.succeeded:
-        return f"versions/{archive_name}"
-    else:
-        return None
+        return archive_path
+    return None
