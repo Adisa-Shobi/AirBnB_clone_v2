5fbef914d3c38ba27cc77870dd94018f58c55860
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-28 16:12:36 +0000

    Update: '1-pack_web_static.py' documentation docstrings changed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" This fabric file is used to simplify deployment of web_static
+''' This fabric file is used to simplify deployment of web_static
 
     Functions:
         do_pack - creates an archive for of the webstatic in versions
                   folder
-"""
+'''
 from fabric.api import *
 from datetime import datetime
 
 
 def do_pack():
-    """ Creates archive of the web_static folder in
+    ''' Creates archive of the web_static folder in
         Format:
             versions/web_static_<year><month><day><hour><minute><second>.tgz
-    """
+    '''
     local("mkdir -p versions")
     archive_path = f"\
 versions/web_static_{datetime.now().strftime('%Y%m%d%H%M%S')}.tgz"
