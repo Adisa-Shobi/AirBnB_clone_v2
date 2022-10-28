8cb7c518d384bf28ec4971bbf8adafbc22431bb2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-10-28 15:17:02 +0000

    Update: '1-pack_web_static.py' modified to fit pycodestyle

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,12 +4,14 @@ from fabric.api import *
 from datetime import datetime
 from fabric.decorators import runs_once
 
+
 @runs_once
 def do_pack():
     """ Creates archive of the web_static folder in
     versions/web_static_<year><month><day><hour><minute><second>.tgz"""
     created_at = datetime.now()
-    archive_path = f"versions/web_static_{created_at.strftime('%Y%m%d%H%M%S')}.tgz"
+    archive_path =
+    f"versions/web_static_{created_at.strftime('%Y%m%d%H%M%S')}.tgz"
     local("mkdir -p versions")
     print(f"Packing web_static to {archive_path}")
     result = local(f"tar -cvzf {archive_path} web_static")
