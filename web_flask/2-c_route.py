c9c7d2441d37f08772f259c32eed15628d8e41eb
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-17 08:14:03 +0000

    Create: '2-c_route.py' adds new route which accepts parametes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Script runs flask web application listening on 0.0.0.0
+and port 5000
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    '''
+    Default route for mimimal flask application
+    '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    Default hbnb route for flask application
+    '''
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def route_c(text):
+    '''Route accepts parameter and returns manipulated version
+    '''
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
