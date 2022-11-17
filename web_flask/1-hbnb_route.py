d7690d2ad898ef87a76326cff8b88f7e9e1223f7
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-17 08:02:26 +0000

    Create: '1-hbnb_route.py' new /hbhnb route added

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Script runs flask web application listening on 0.0.0.0
+and port 5000
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    '''
+    Default route for mimimal flask application
+    '''
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    Default hbnb route for flask application
+    '''
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
