c23738316916afb3318526edfb4bc425b4372682
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-17 12:48:37 +0000

    Create: '5-number_template.py' new route rnders template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+'''
+Script runs flask web application listening on 0.0.0.0
+and port 5000
+'''
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    '''
+    Default route for mimimal flask application
+    '''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''
+    Default hbnb route for flask application
+    '''
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def route_c(text):
+    '''Route accepts parameter and returns manipulated version
+    '''
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def route_python(text="is cool"):
+    '''Route accepts parameter and returns manipulated version
+    preceded by Python
+    '''
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>')
+def number_route(n):
+    '''Route returns parameter only if it is an integer
+    '''
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    '''Render a template if an integer parameter is supplied
+    '''
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
