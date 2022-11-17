4007cae75cc9cc2ac9bf502f6f2952e5df241404
Adisa-Shobi <s.oadisa.dev@gmail.com>
2022-11-17 07:38:02 +0000

    Create: '0-hello_route.py, __init__.py' runs a simple flask application, and initializes a module respectively

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Script runs flask web application listening on 0.0.0.0
+and port 5000
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    '''
+    Default route for mimimal flask application
+    '''
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
